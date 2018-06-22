a51805095b4875380cfef787591a6d3b7c8ef169
liamq82 <liamq82@gmail.com>
2018-06-22 22:15:34 +0100

    refactor unit test

--- Tests/offeringControllerTests.js
+++ Tests/offeringControllerTests.js
@@ -3,26 +3,27 @@ var should = require('should'),
 
 describe('Offering Controller Tests:', function () {
     describe('Post', function () {
-        it('should not allow an empty cusip ID on POST', function () {
-            var Offering = function (offering) {
-                this.save = function () {
 
-                }
+        var Offering = function (offering) {
+            this.save = function () {
             }
+        }
+        var offeringController = require('../controller/offeringController')(Offering)
+        var res;
 
+        beforeEach(function () {
+            res = {
+                status: sinon.spy(),
+                send: sinon.spy()
+            }
+        })
+
+        it('should not allow an empty cusip ID on POST', function () {
             var req = {
                 body: {
                     description: 'My offering for testing.'
                 }
             }
-
-            var res = {
-                status: sinon.spy(),
-                send: sinon.spy()
-            }
-
-            var offeringController = require('../controller/offeringController')(Offering)
-
             offeringController.post(req, res)
 
             res.status.calledWith(400).should.equal(true, 'Bad status' + res.status.args[0][0])
@@ -30,20 +31,12 @@ describe('Offering Controller Tests:', function () {
         })
 
         it('should save valid offerings', function () {
-            var Offering = function (offering) {
-                this.save = function () { }
-            }
             var req = {
                 body: {
                     cusipId: '12345'
                 }
             }
-            var res = {
-                status: sinon.spy(),
-                send: sinon.spy()
-            }
 
-            var offeringController = require('../controller/offeringController')(Offering)
             offeringController.post(req, res)
             res.status.calledWith(201).should.equal(true, 'Valid offering not saved successfully')
             res.send.calledOnce.should.equal(true, 'Send was not called for valid offering')
