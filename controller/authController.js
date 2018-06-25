073089e103cfa254cdf21f11c58e07e055a02424
liamq82 <liamq82@gmail.com>
2018-06-25 15:37:47 +0100

    refactor authentication error handling

--- controller/authController.js
+++ controller/authController.js
@@ -1,20 +1,16 @@
 var authController = function (User, jwt) {
 
     var authenticateUser = function (req, res, next) {
-        if (!req.header('token')) {
+        var token = req.header('token')
+        var payload
+        try {
+            payload = jwt.decode(token, '123')
+        }
+        catch (err) {
             res.status(401)
-            res.send({ message: 'Unauthorized. Missing authorization token.' })
-        } else {
-            var token = req.header('token')
-            var payload
-            try {
-                payload = jwt.decode(token, '123')
-            }
-            catch (err) {
-                res.status(401)
-                res.send({ message: 'Unauthorized. Auth token invalid.' })
-                return
-            }
+            res.send({ message: 'Unauthorized. Auth token invalid.' })
+        }
+        if (payload) {
             req.userId = payload.sub
             next()
         }
