783a426f27d31cf6651bca820d89c26ab4c787b0
liamq82 <liamq82@gmail.com>
2018-06-25 22:11:25 +0100

    unit test valid password passed to login controller

--- Tests/loginControllerTests.js
+++ Tests/loginControllerTests.js
@@ -4,12 +4,14 @@ var should = require('chai').should(),
 describe('Login Controller:', function () {
 
     describe('Login', function () {
-
+        var mockValidUser = { _id: '143tdc3n4coeb34', password: 'abc123defxys' }
+        var mockToken = 'utoc3n234234b23nc'
         var mockModelThatThrowsDatabaseError
         var mockModelThatReturnsUser
         var jwt
         var bcrypt
         var mockBcryptThatReturnsInvalidPassword
+        var mockBcryptThatReturnsValidPassword
         var req
         var res
 
@@ -21,13 +23,22 @@ describe('Login Controller:', function () {
             }
             mockModelThatReturnsUser = {
                 findOne(query, cb) {
-                    cb(undefined, { password: 'abc123defxys' })
+                    cb(undefined, mockValidUser)
                 }
             }
-            jwt = {}
+            jwt = {
+                encode: function (payload, secret) {
+                    return mockToken
+                }
+            }
             mockBcryptThatReturnsInvalidPassword = {
                 compare: function (invalidPassword, passwordToCompareTo, cb) {
                     cb(undefined, false)
+                }
+            }
+            mockBcryptThatReturnsValidPassword = {
+                compare: function (invalidPassword, passwordToCompareTo, cb) {
+                    cb(undefined, true)
                 }
             }
             bcrypt = {}
@@ -62,6 +73,17 @@ describe('Login Controller:', function () {
             res.send.calledWith({ message: 'invalid email or password' })
 
             res.status.calledOnce.should.be.true
+            res.send.calledOnce.should.be.true
+        })
+
+        it('should send user a token when password is valid', function () {
+            var loginController = require('../controller/loginController')(mockModelThatReturnsUser, jwt, mockBcryptThatReturnsValidPassword)
+            loginController.login(req, res)
+
+            res.status.calledWith(200).should.be.true
+            res.status.calledOnce.should.be.true
+
+            res.send.calledWith({ token: mockToken }).should.be.true
             res.send.calledOnce.should.be.true
         })
     })
