76708f0e4ace8edcd2222de7d54f112577be1f3e
liamq82 <liamq82@gmail.com>
2018-06-25 14:31:55 +0100

    unit test auth controller adds user id to request object if user has a valid token

--- Tests/authControllerTests.js
+++ Tests/authControllerTests.js
@@ -32,5 +32,32 @@ describe('Auth Controller Tests:', function () {
             res.status.calledWith(401).should.be.true
             res.send.calledWith({ message: 'Unauthorized. Missing authorization token.' }).should.be.true
         })
+
+        it('should add user id to request object if header token is valid', function () {
+            var userToken = 'abc123def456'
+            var userId = '123xyz'
+            var req = {
+                header: function (token) {
+                    return userToken
+                }
+            }
+            var res = {
+                status: sinon.spy(),
+                send: sinon.spy()
+            }
+            var jwt = {
+                decode: function (token, secret) {
+                    return { sub: userId }
+                }
+            }
+            var next = sinon.spy()
+            var authController = require('../controller/authController')(User, jwt)
+            authController.authenticateUser(req, res, next)
+
+            req.userId.should.equal(userId)
+            next.calledOnce.should.be.true
+            res.status.notCalled.should.be.true
+            res.send.notCalled.should.be.true
+        })
     })
 })
